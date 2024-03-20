ae38d1f630e42b6851b6cfc3b42f24f5246da94c
itisMilan <abhaymilan8@gmail.com>
2024-03-20 22:55:53 +0530

    Added the search bar with mapbox api

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -3,14 +3,40 @@
     <div class="pt-4 mb-8 relative">
       <input type="text" 
       v-model="searchQuery" 
+      @input="getSearchResults"
       placeholder="Search for city" 
-      class="py-2 px-1 w-full bg-transparent border-b border-gray-500 focus:outline-none focus:border-weather-secondary focus:outline-none focus:shadow-[0_1px_0_0_#004E71]">
+      class="py-2 px-1 w-full bg-transparent border-b  focus:outline-none focus:border-weather-secondary focus:outline-none focus:shadow-[0_1px_0_0_#004E71]">
     </div>
+    <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[146px] " v-if="mapboxSearchResults">
+    <li v-for="searchResult in mapboxSearchResults" 
+  :key="searchResult.id" class="py-2 cursor-pointer">
+{{ searchResult.place_name }}
+</li></ul>
 
   </main>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+const mapboxApiKey = import.meta.env.VITE_APP_PUBLIC_KEY_WEATHER;
+console.log(mapboxApiKey)
 const searchQuery=ref("")
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+
+const getSearchResults = () => {
+  console.log(mapboxApiKey)
+  clearTimeout(queryTimeout.value)
+  queryTimeout.value = setTimeout(async () => {
+    if(searchQuery.value !== ''){
+      const result=await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxApiKey}&types=place`)
+      mapboxSearchResults.value=result.data.features;
+      return;
+    }
+    mapboxSearchResults.value = null
+    console.log(mapboxSearchResults.value)
+  }, 300)
+}
 </script>
 
