b6b9b8fcc478e70c53195812dc29c15c1e4a6f8b
itisMilan <abhaymilan8@gmail.com>
2024-03-20 21:56:16 +0530

    Setup the homeview and also added axios

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
-  
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" 
+      v-model="searchQuery" 
+      placeholder="Search for city" 
+      class="py-2 px-1 w-full bg-transparent border-b border-gray-500 focus:outline-none focus:border-weather-secondary focus:outline-none focus:shadow-[0_1px_0_0_#004E71]">
+    </div>
+
   </main>
 </template>
+
+<script setup>
+const searchQuery=ref("")
+</script>
+
