e3acd221fc31d2fd755413b122a49a7856ef2993
itisMilan <abhaymilan8@gmail.com>
2024-03-21 18:13:21 +0530

    Added the city list

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,51 +1,93 @@
 <template>
   <main class="container text-white">
     <div class="pt-4 mb-8 relative">
-      <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search for city"
-        class="py-2 px-1 w-full bg-transparent border-b  focus:outline-none focus:border-weather-secondary focus:outline-none focus:shadow-[0_1px_0_0_#004E71]">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
+        <p class="py-2" v-if="searchError">
+          Sorry, something went wrong, please try again.
+        </p>
+        <p
+          class="py-2"
+          v-if="!searchError && mapboxSearchResults.length === 0"
+        >
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in mapboxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)"
+          >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
+      </ul>
     </div>
-    <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[146px] "
-      v-if="mapboxSearchResults">
-      <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-        {{ searchResult.place_name }}
-      </li>
-      <p v-if="searchError">Sorry, something went wrong, please try again.</p>
-      <p v-if="!searchError && !mapboxSearchResults">No results match your query, try a different term.</p>
-      <template v-else>
-        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-          {{ searchResult.place_name }}</li>
-      </template>
-    </ul>
-
+    <div class="flex flex-col gap-4">
+      <Suspense>
+        <CityList />
+        <template #fallback>
+          <p>Loading...</p>
+        </template>
+      </Suspense>
+    </div>
   </main>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import axios from 'axios';
-const mapboxApiKey = import.meta.env.VITE_APP_PUBLIC_KEY_WEATHER;
-console.log(mapboxApiKey)
-const searchQuery = ref("")
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import CityList from "../components/CityList.vue";
+
+const router = useRouter();
+const previewCity = (searchResult) => {
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: "cityView",
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
+
+const mapboxAPIKey = import.meta.env.VITE_APP_PUBLIC_KEY_WEATHER 
+const searchQuery = ref("");
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
-  console.log(mapboxApiKey)
-  clearTimeout(queryTimeout.value)
+  clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
-    if (searchQuery.value !== '') {
+    if (searchQuery.value !== "") {
       try {
-        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxApiKey}&types=place`)
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        );
         mapboxSearchResults.value = result.data.features;
-
-      }
-      catch {
+      } catch {
         searchError.value = true;
       }
+
       return;
     }
-    mapboxSearchResults.value = null
-    console.log(mapboxSearchResults.value)
-  }, 300)
-}
+    mapboxSearchResults.value = null;
+  }, 300);
+};
 </script>
+
+<style lang="scss" scoped></style>
